49de9c40b62cdfd6a06dd22a5a9c5ce2fe86c7c4
Wojciech K <wojtekkk@protonmail.com>
2022-01-21 20:55:37 +0100

    currencies created

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 3.1,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.1,
+    },
+    {
+        short: "GBP",
+        name: "Funt szterling",
+        rate: 5.1,
+    },
+];
