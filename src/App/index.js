1cd3a5d1b7f3f45132211e21b77eafa6db8f0c13
Wojciech K <wojtekkk@protonmail.com>
2022-01-21 20:55:10 +0100

    App component created with its styles

---
+++ src/App/index.js
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+import './App.css';
+import {Form} from './Form';
+import {currencies} from './currencies'; 
+
+function App() {
+
+  const [result, setResult] = useState();
+  
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+    
+    .find(({short}) => short === currency)
+    .rate;
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount * rate,
+      currency,
+    });
+  }
+
+  return (
+    <div className="app">
+      <Form
+        result={result}
+        calculateResult={calculateResult}
+      />
+    </div>
+  );
+};
+
+export default App;
