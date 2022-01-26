f68adfac13accbfc86bfd1be744c5d81df4ac71f
Wojciech K <wojtekkk@protonmail.com>
2022-01-26 11:43:14 +0100

    const result and calculateResult transferred

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -3,82 +3,98 @@ import {currencies} from "../currencies";
 import {Result} from "./Result";
 import "./style.css";
 
-export const Form = ({calculateResult, result}) => {
-    const [currency, setCurrency] = useState(currencies[2].short);
-    const [amount, setAmount] = useState("");
+export const Form = () => {
+  const [result, setResult] = useState();
+  
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+    
+    .find(({short}) => short === currency)
+    .rate;
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount * rate,
+      currency,
+    });
+  };
+    
+  const [currency, setCurrency] = useState(currencies[2].short);
+    
+  const [amount, setAmount] = useState("");
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        calculateResult(currency,amount);
-    }
+  const onSubmit = (event) => {
+      event.preventDefault();
+      calculateResult(currency,amount);
+  };
 
-    const onReset = () => {
-      setAmount("");
-      setCurrency((currencies[2].short));
-      calculateResult(result !== undefined);
-    };
+  const onReset = (event) => {
+    event.preventDefault();
+    setAmount("");
+    setCurrency(currencies[2].short);
+    setResult();
+  };
 
-    return (
-        <form 
-          className="form" 
-          onSubmit={onSubmit}
-          onReset={onReset}
-        >
-          <fieldset className="form__fieldset">
-            <legend className="form__legend">
-            Kalkulator walutowy by Wojciech K
-            </legend>
-              <label>
-                <span className="form__labelElement">
-                1. Wybierz walutę z listy:
-                </span>
-                  <select 
-                    className="form__field" 
-                    value={currency} 
-                    onChange={({target}) => setCurrency(target.value)} 
-                    autoFocus
-                  >
-                    {currencies.map((currency => (
-                      <option 
-                        key={currency.short} 
-                        value={currency.short}
-                      >
-                        {currency.name}
-                      </option>
-                    )))}
-                  </select>
-                </label>
+  return (
+    <form 
+      className="form" 
+      onSubmit={onSubmit}
+      onReset={onReset}
+    >
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">
+        Kalkulator walutowy by Wojciech K
+        </legend>
+          <label>
+            <span className="form__labelElement">
+            1. Wybierz walutę z listy:
+            </span>
+            <select 
+              className="form__field" 
+              value={currency} 
+              onChange={({target}) => setCurrency(target.value)} 
+              autoFocus
+            >
+              {currencies.map((currency => (
+                <option 
+                  key={currency.short} 
+                  value={currency.short}
+                >
+                  {currency.name}
+                </option>
+              )))}
+            </select>
+          </label>
+          
+          <label>
+            <span className="form__labelElement">
+            2. Wpisz kwotę w wybranej walucie:*
+            </span>
+              <input 
+                className="form__field" 
+                value={amount} 
+                onChange={({target}) => setAmount(target.value)} 
+                type="number" 
+                placeholder="0.01" 
+                step="0.01"
+                min="0.01" 
+                max="1000000" 
+                required
+                autoFocus
+              />
+          </label>
 
-              <label>
-                <span className="form__labelElement">
-                2. Wpisz kwotę w wybranej walucie:*
-                </span>
-                  <input 
-                    className="form__field" 
-                    value={amount} 
-                    onChange={({target}) => setAmount(target.value)} 
-                    type="number" 
-                    placeholder="0.01" 
-                    step="0.01"
-                    min="0.01" 
-                    max="1000000" 
-                    required
-                    autoFocus
-                  />
-              </label>
+          <p className="form__paragraph">Pole * nie może być puste</p>
 
-              <p className="form_paragraph">Pole * nie może być puste</p>
+          <button className="form__button">
+          Przelicz na złotówki (PLN):
+          </button>
 
-              <button className="form__button">
-              Przelicz na złotówki (PLN):
-              </button>
-
-              <button className="form__button" type="reset">
-              Wyczyść
-              </button>
+          <button className="form__button" type="reset">
+          Wyczyść
+          </button>
                   
-              <Result result={result} />
-          </fieldset>
-        </form>
-      )
+          <Result result={result} />
+      </fieldset>
+    </form>
+  )
 };
--- src/App/index.js
+++ src/App/index.js
@@ -1,30 +1,11 @@
-import {useState} from 'react';
 import './App.css';
 import {Form} from './Form';
-import {currencies} from './currencies'; 
 
 function App() {
-
-  const [result, setResult] = useState();
   
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-    
-    .find(({short}) => short === currency)
-    .rate;
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount * rate,
-      currency,
-    });
-  }
-
   return (
     <div className="app">
-      <Form
-        result={result}
-        calculateResult={calculateResult}
-      />
+      <Form/>
     </div>
   );
 };
