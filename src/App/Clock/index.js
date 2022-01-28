c8fa5a4ef35ca545b1526da53aff296c1e6f7c8e
Wojciech K <wojtekkk@protonmail.com>
2022-01-28 18:30:36 +0100

    new component created

---
+++ src/App/Clock/index.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import "./style.css";
+
+export const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    },[]);
+
+    return (
+        <div className="clock">
+            Dzisiaj jest 
+            {" "}
+            {date.toLocaleString(undefined, {
+                weekday: "long",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                day: "numeric",
+                month: "long"
+            })}
+        </div>
+    )
+};
--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Form} from './Form';
+import {Clock} from './Clock';
 
 function App() {
   
   return (
     <div className="app">
+      <Clock/>
       <Form/>
     </div>
   );
