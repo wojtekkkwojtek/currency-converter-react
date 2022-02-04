ea063ff0698486357ac1377d426d9b4d2bca5878
Wojciech K <wojtekkk@protonmail.com>
2022-02-04 09:54:54 +0100

    GlobalStyle.js created instead of index.css

---
+++ src/App/GlobalStyle.js
@@ -0,0 +1,25 @@
+import {createGlobalStyle} from "styled-components";
+import background from "./background.jpg";
+
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+    box-sizing: inherit;
+    }
+
+    #root {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        background-image: url("${background}");
+        background-size:cover;
+        background-position: center;
+        background-color: rgb(145, 208, 233);
+        font-family: 'Pangolin', cursive;
+    }
+`;
