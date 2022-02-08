0c68d62286314868ce091242ffb86eabda2e406d
Wojciech K <wojtekkk@protonmail.com>
2022-02-08 13:31:23 +0100

    const formattedDate created

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,21 +1,24 @@
 import {Wrapper} from "./styled";
 import {useCurrentDate} from "./useCurrentDate";
 
+const formattedDate = (date) => 
+    date.toLocaleString(undefined, {
+        weekday: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        day: "numeric",
+        month: "long"
+    });
+
 export const Clock = () => {
-    const date = useCurrentDate();
+    const date = useCurrentDate(); 
     
     return (
         <Wrapper>
             Dzisiaj jest 
             {" "}
-            {date.toLocaleString(undefined, {
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-                day: "numeric",
-                month: "long"
-            })}
+            {formattedDate(date)}
         </Wrapper>
-    )
+    );
 };
